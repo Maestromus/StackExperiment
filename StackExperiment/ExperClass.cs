b62feb206e557396adbaa051f14a6cdcc683b1b5
Dima <dima.aidinov@gmail.com>
2023-03-05 19:55:32 +0200

    first push

---
+++ StackExperiment/ExperClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackExperiment
+{
+    public class ExperClass
+    {
+
+        public static bool CheckBrackets(string input)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in input)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ')' || c == '}' || c == ']')
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    char last = stack.Peek();
+
+                    if ((c == ')' && last == '(') ||
+                        (c == '}' && last == '{') ||
+                        (c == ']' && last == '['))
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ StackExperiment/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using StackExperiment;
+
+public class ReadFromFile : ExperClass
+{
+    static void Main(string[] args)
+    {
+
+
+        // Считываем содержимое файла
+        string[] lines = File.ReadAllLines(@"D:\readText\Stack.txt");
+
+
+        // Разбиваем содержимое файла на массив строк
+        // string[] lines = text.Split('\n');
+
+
+        // Проходим по всем строкам файла
+        foreach (string line in lines)
+        {
+            // Выполняем математическую операцию над примером и выводим результат в консоль
+            Console.WriteLine(line + " = " + CheckBrackets(line));
+        }
+
+    }
+}
